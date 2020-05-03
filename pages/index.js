89960b6d45aba54f0771d1e0280f0009f18ca45b
mkochakian <mattk4@me.com>
2020-05-03 14:02:16 -0700

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import { useState, useEffect } from 'react';
+import data from '../data';
+let cn = require('classnames');
 
 export default ({ }) => {
+  const toggleExpand = className => document.getElementById(className).classList.contains('expand') ? document.getElementById('cat1').classList.remove('expand') : document.getElementById('cat1').classList.add('expand')
   return(
     <div className='app'>
-      resources
+      <nav className='sc--sidebar'>
+        <ul>
+          {Object.keys(data).map((item, index) => {
+            data[item].isArray()
+            ? <li id={`cat_${index}`}>
+                <div className='row-fs-c' onClick={() => toggleExpand(`cat_${index}`)}><div className='col-c-c'>▶︎</div>category</div>
+                <li className='active row-fs-c'>test</li>
+                <li className='row-fs-c'>test</li>
+                <li className='row-fs-c'>test</li>
+              </li>
+            : <li className='row-fs-c'>{item}</li>
+          })}
+        </ul>
+      </nav>
+      <section className='sc--main col'>
+
+      </section>
     </div>
   )
 }
