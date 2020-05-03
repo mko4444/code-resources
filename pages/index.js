5614fae2f68c48c112a503d54d0e12c84714b987
mkochakian <mattk4@me.com>
2020-05-03 15:33:35 -0700

    fixes for layout

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,24 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import data from '../data';
 let cn = require('classnames');
 
 export default ({ }) => {
-  const toggleExpand = className => {
-    console.log(className)
-    document.getElementById('cat_' + className).classList.contains('expand') ? document.getElementById('cat_' + className).classList.remove('expand') : document.getElementById('cat_' + className).classList.add('expand')
-  }
+  const toggleExpand = className => document.getElementById('cat_' + className).classList.contains('expand') ? document.getElementById('cat_' + className).classList.remove('expand') : document.getElementById('cat_' + className).classList.add('expand')
   return(
     <div className='app'>
-      <nav className='sc--sidebar'>
-        <ul>
-          {Object.keys(data).map((x, index) => {
-            return Array.isArray(data[x])
-            ? <li id={`cat_${x}`}>
-                <div className='row-fs-c' onClick={() => toggleExpand(x)}><div className='col-c-c'>▶︎</div>{x}</div>
-                <ul>
-                  {Object.keys(data[x]).map((y, index) => {
-                    console.log(y)
-                    return(
-                      <li className='row-fs-c'>{data[x][y].name}</li>
-                    )
-                  }
-                  )}
-                </ul>
-              </li>
-            : <li className='row-fs-c'>{x}</li>
-          })}
-        </ul>
-      </nav>
+      <ul className='sc--sidebar'>
+        {Object.keys(data).map((x, index) => {
+          return !Array.isArray(data[x])
+          ? <li id={`cat_${x}`}>
+              <div className='row-fs-c' onClick={() => toggleExpand(x)}><div className='col-c-c'>▶︎</div><span>{x}</span></div>
+              <ul>
+                {Object.keys(data[x]).map((y, index) => <li className='row-fs-c'><span>{data[x][y].name}</span></li>)}
+              </ul>
+            </li>
+          : <li className='row-fs-c'><span>{x}</span></li>
+        })}
+      </ul>
       <section className='sc--main col'>
         <h1>Category</h1>
         <ul>
