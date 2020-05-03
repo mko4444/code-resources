84710ad7fffc4635e8cbe4c412b313fd6bf4dfe5
vdthatte <rockstarvd@gmail.com>
2020-05-03 16:27:37 -0400

    added test data

--- data.js
+++ data.js
@@ -1,1 +1,56 @@
 // data goes here
+{
+	'ZIP' : [
+		{
+			'name': 'Zip',
+			'desc': 'Framework for zipping and unzipping files.',
+			'git_url': 'https://github.com/marmelroy/Zip'
+		},
+		{
+			'name': 'Zip Foundation',
+			'desc': 'A library to create, read and modify ZIP archive files.',
+			'git_url': 'https://github.com/weichsel/ZIPFoundation'
+		}
+	],
+	'Date' : [
+		{
+			'name': 'AnyDate',
+			'desc': 'Date & Time API inspired from Java 8 DateTime API.',
+			'git_url': 'https://github.com/Kawoou/AnyDate'
+		},
+		{
+			'name': 'Chronology',
+			'desc': 'Building a better date/time library.',
+			'git_url': 'https://github.com/davedelong/time'
+		},
+				{
+			'name': 'DateHelper',
+			'desc': 'Library for dealing with `NSDate`, `NSCalendar`, `NSDateComponents`, and `NSTimeInterval`.',
+			'git_url': 'https://github.com/melvitax/DateHelper'
+		},
+				{
+			'name': 'Datez',
+			'desc': 'Date & Time API inspired from Java 8 DateTime API.',
+			'git_url': 'https://github.com/SwiftKitz/Datez'
+		}
+	],
+	'Device' : [
+		{
+			'name': 'Device',
+			'desc': 'Light weight tool for detecting the current device and screen size.',
+			'git_url': 'https://github.com/Ekhoo/Device'
+		},
+				{
+			'name': 'Device.swift',
+			'desc': 'Super-lightweight library to detect used device.',
+			'git_url': 'https://github.com/schickling/Device.swift'
+		},
+				{
+			'name': 'DeviceKit',
+			'desc': 'DeviceKit is a value-type replacement of UIDevice.',
+			'git_url': 'https://github.com/devicekit/DeviceKit'
+		},
+	]
+}
+
+
