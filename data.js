d08d91dd85820e425129c461cd1e550671eb19a9
mkochakian <mattk4@me.com>
2020-05-03 15:21:42 -0700

    sidebar + main

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-{
+export default {
     "Newsletter": [
        {
           "name": "Swift Developments",
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,47 @@ import data from '../data';
 let cn = require('classnames');
 
 export default ({ }) => {
-  const toggleExpand = className => document.getElementById(className).classList.contains('expand') ? document.getElementById('cat1').classList.remove('expand') : document.getElementById('cat1').classList.add('expand')
+  const toggleExpand = className => {
+    console.log(className)
+    document.getElementById('cat_' + className).classList.contains('expand') ? document.getElementById('cat_' + className).classList.remove('expand') : document.getElementById('cat_' + className).classList.add('expand')
+  }
   return(
     <div className='app'>
       <nav className='sc--sidebar'>
         <ul>
-          {Object.keys(data).map((item, index) => {
-            data[item].isArray()
-            ? <li id={`cat_${index}`}>
-                <div className='row-fs-c' onClick={() => toggleExpand(`cat_${index}`)}><div className='col-c-c'>▶︎</div>category</div>
-                <li className='active row-fs-c'>test</li>
-                <li className='row-fs-c'>test</li>
-                <li className='row-fs-c'>test</li>
+          {Object.keys(data).map((x, index) => {
+            return Array.isArray(data[x])
+            ? <li id={`cat_${x}`}>
+                <div className='row-fs-c' onClick={() => toggleExpand(x)}><div className='col-c-c'>▶︎</div>{x}</div>
+                <ul>
+                  {Object.keys(data[x]).map((y, index) => {
+                    console.log(y)
+                    return(
+                      <li className='row-fs-c'>{data[x][y].name}</li>
+                    )
+                  }
+                  )}
+                </ul>
               </li>
-            : <li className='row-fs-c'>{item}</li>
+            : <li className='row-fs-c'>{x}</li>
           })}
         </ul>
       </nav>
       <section className='sc--main col'>
-
+        <h1>Category</h1>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <h2>Sub-category</h2>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </section>
     </div>
   )
