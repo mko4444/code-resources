3f6eaa56105a45678966fd9e8d9c6a0ce10118ff
vdthatte <rockstarvd@gmail.com>
2020-05-04 00:25:47 -0400

    new structure of organization

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,33 @@ const App = ({ data, query }) => {
   const toggleExpand = className => document.getElementById('cat_' + className).classList.contains('expand') ? document.getElementById('cat_' + className).classList.remove('expand') : document.getElementById('cat_' + className).classList.add('expand')
   let [ categories, setCategories ] = useState(parseCategories(data.categories)),
       router = useRouter();
-
+  let [ projects, setProjects ] = useState({});
 
   useEffect(() => {
-    console.log(data.projects.filter(p => p.category === query.id));
-    
+    console.log(query.cat);
+    loadPage("audio");
     // expands the category that contains the active subcategory
     !!query.cat && document.getElementById(`cat_${query.cat}`).classList.add('expand')
   }, [])
+
+
+  function loadPage(e){
+
+    var projects = {}
+    var parents = data.categories.filter(p => p.parent === e);
+    
+    for (var i = 0; i < parents.length; i++) {
+      var d = data.projects.filter(p => p.category === parents[i].id)
+      projects[parents[i].id] = [];
+      for (var j = 0; j < parents.length; j++) {
+        projects[parents[i].id].push(d[j]);
+      }
+    }
+
+    console.log(projects);
+    setProjects(projects);
+
+  }
 
 
   let currentCat = data.categories.filter(x => x.id === query.id)[0] || false
@@ -42,18 +61,8 @@ const App = ({ data, query }) => {
           let name = item.toLowerCase().replace(' ', '-') // we want to clean the strings for the url query
           return(
             <li id={`cat_${name}`}>
-              <div className='row-fs-c' onClick={() => toggleExpand(name)}><div className='col-c-c'>▶︎</div><span>{name}</span></div>
-                <ul>
-                  {categories[item].map((y, index) =>
-                    <li
-                      onClick={() => router.push({pathname: '/', query: { cat: name, id: y.id }})}
-                      className={cn(['row-sb-c', {'active': query.id === y.id}])}
-                    >
-                      <span>{y.title}</span>
-                      <span style={{opacity: .5}}>{data.projects.filter(x => x.category === y.id).length}</span>
-                    </li>
-                  )}
-                </ul>
+              {/* <div className='row-fs-c' onClick={() => router.push({pathname: '/', query: { cat: name }})}><span>{name}</span></div> */}
+              <div className='row-fs-c' onClick={() => loadPage(name)}><span>{name}</span></div>
             </li>
           )
         })
@@ -61,11 +70,27 @@ const App = ({ data, query }) => {
       </ul>
       <section className='sc--main col'>
         <h1>{currentCat.title || 'Projects'}</h1>
-        <ul>
-          {data.projects.filter(p => p.category === query.id).map((p, i) =>
-            <li><Cell name={p.title} href={p.homepage} desc={p.description}/></li>
-          )}
-        </ul>
+        {Object.keys(projects).map((p, i) => {
+
+          return(
+            <div>
+              <p>{p}</p>
+              <ul>
+                {
+                  projects[p].map((project, i) => {
+                    if (project != undefined) {
+                      return (
+                        <li><Cell name={project.title} href={project.homepage} desc={project.description}/></li>
+                      )
+                    }
+                  })
+                }
+              </ul>
+            </div>
+          )
+
+        })
+        }
       </section>
     </div>
   )
